368470a49874141f9c1c634b358481f6c63a1bf8
“elisejonkers” <elisejonkers18@hotmail.com>
2024-02-19 14:23:29 -0600

    added login and authentication methods

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,8 +1,27 @@
-import { useState } from "react"
+import axios, { AxiosError, AxiosHeaders, AxiosResponse } from "axios"
+import { useContext, useState } from "react"
+import { AuthContext } from "../context/auth.context"
 
 interface LoginState {
     email: string,
     password: string
+}
+
+interface AuthResponseData {
+    authToken: string
+}
+
+interface AuthResponse {
+    data: {
+        authToken: string,
+    },
+    status: number,
+    statusText: string,
+    headers: AxiosHeaders
+}
+
+interface ErrorResponse {
+    message: string
 }
 
 const LogIn: React.FC = () => {
@@ -10,11 +29,40 @@ const LogIn: React.FC = () => {
         email: "",
         password: ""
     })
+    const [errorMessage, setErrorMessage] = useState<string | undefined>()
+
+    const { storeToken, authenticateUser } = useContext(AuthContext)
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setLogIn({...logIn, [name]: value})
+    }
+
+    const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        axios
+            .post("http://localhost:5005/auth/login", logIn)
+            .then((response: AxiosResponse<AuthResponseData>) => {
+                console.log("Login approved")
+                console.log("This is the JTW token", response.data.authToken)
+                storeToken(response.data.authToken)
+                authenticateUser()
+            })
+            .catch((error: AxiosError<ErrorResponse>) => {
+                console.log(error)
+                if (error.response) {
+                    setErrorMessage(error.response.data.message)
+                } else {
+                    setErrorMessage("An error occured")
+                }
+            })
+    }
 
     return (
         <div>
             <h1>Log In</h1>
-            <form action="">
+            <form onSubmit={handleLoginSubmit}>
                 <label htmlFor="">
                     Email
                     <input 
@@ -22,6 +70,8 @@ const LogIn: React.FC = () => {
                     name="email"
                     placeholder="Enter your email here"
                     required={true}
+                    value={logIn.email}
+                    onChange={handleInputChange}
                      />
                 </label>
                 <label htmlFor="">
@@ -31,6 +81,8 @@ const LogIn: React.FC = () => {
                     name="password"
                     placeholder="Enter your password here"
                     required={true}
+                    value={logIn.password}
+                    onChange={handleInputChange}
                      />
                 </label>
                 <button>Log in</button>
