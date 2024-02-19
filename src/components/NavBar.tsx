8d1c74f374d867bd79178b560705b41a908dc65e
“elisejonkers” <elisejonkers18@hotmail.com>
2024-02-19 14:23:03 -0600

    updated NavBar with logout option after login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 function NavBar() {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext)
   return (
     <div className="navbar-test">
       <Link to={"/"}>Little Moments</Link>
-      <p>My albums</p>
-      <Link to={"/login"}>Log in</Link>
-      <Link to={"/signup"}>Sign up</Link>
+      {isLoggedIn && (
+        <>
+          <p>My albums</p>
+          <button onClick={logOutUser}>Logout</button>
+        </>
+      )}
+
+      {!isLoggedIn && (
+        <>
+          <Link to={"/login"}>Log in</Link>
+          <Link to={"/signup"}>Sign up</Link>
+        </>
+      )}
     </div>
   );
 }
