6291a00d68dc16ee8fe3d44ba1bd1d9b2f83fc9d
“elisejonkers” <elisejonkers18@hotmail.com>
2024-02-21 15:46:36 -0600

    deleted ObjectId from interface

--- src/components/AddAlbumForm.tsx
+++ src/components/AddAlbumForm.tsx
@@ -3,7 +3,6 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 
 interface Album {
-    //_id: ObjectId,
     name: string,
     dateOfBirth: Date,
     place: string,
